038113fcab7c0b5d45ef76ce8b9fe1e698bcc3f6
Mike Ludemann <mike-ludemann@hotmail.de>
2020-02-20 14:14:24 +0100

    Setup complete - vue-textfield

---
+++ src/components/textfield.vue
@@ -0,0 +1,49 @@
+<template lang="html">
+
+	<div class="textfield--custom">
+		<input type="text" class="form" v-bind:name="name" @input="changeHandling" @change="changeHandling" />
+	</div>
+
+</template>
+
+<script lang="js">
+
+	export default {
+		name: 'textfield',
+		props: {
+			name: String
+		},
+		mounted () {
+
+		},
+		data () {
+			return {
+
+			}
+		},
+		methods: {
+			changeHandling ({ type, target }) {
+			const event = {
+				type,
+				isCheckbox: target.type === 'checkbox',
+				target: {
+					value: target.value,
+					checked: target.checked
+				}
+			}
+			console.log(`${event.type}: ${event.isCheckbox ? event.target.checked : event.target.value}`);
+			}
+		},
+		computed: {
+
+		}
+	}
+
+
+</script>
+
+<style scoped lang="scss">
+	.textfield--custom {
+
+	}
+</style>
